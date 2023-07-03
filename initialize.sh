abf983053d55e0d76b67c67296c32ea16b5d146e
canalun <i.am.kanaru.sato@gmail.com>
2023-07-04 01:57:30 +0900

    initialize for cpp

--- initialize.sh
+++ initialize.sh
@@ -4,13 +4,14 @@
 cd $1
 # $2: round name
 mkdir $2 && cd $2
+# $3: file extension
 
 for i in {a..f}; do
-  touch $i.js
-  cat ../template.js > $i.js
+  touch $i.$3
+  cat ../template.$3 > $i.$3
 done
 
 touch input.txt
 touch output.txt
 
-cp ../../check.sh check.sh
+cp ../../check$3.sh check$3.sh
